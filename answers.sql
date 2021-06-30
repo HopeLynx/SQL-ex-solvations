299180a2872e8fbb7c742f7d416d2328ce93ea08
HopeLynx <grigoriy.sizov@gmail.com>
2021-06-30 19:45:26 +0300

    added first six solvations

--- answers.sql
+++ answers.sql
@@ -1,1 +1,20 @@
+-- 1
+SELECT model,speed,hd FROM PC WHERE price < 500
 
+--2
+SELECT DISTINCT maker from Product WHERE type = 'Printer'
+
+-- 3
+SELECT model,ram,screen FROM Laptop WHERE price > 1000
+
+-- 4
+SELECT * FROM Printer WHERE color = 'y'
+
+-- 5
+SELECT model,speed,hd FROM PC WHERE price < 600 AND  (cd='12x' or cd = '24x')
+
+-- 6
+SELECT DISTINCT Product.maker,Laptop.speed
+FROM Laptop 
+JOIN Product ON Product.model = Laptop.model
+WHERE Laptop.hd >= 10
