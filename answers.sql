378839eb505961c1a3144d90b6c74d7d686ed478
HopeLynx <grigoriy.sizov@gmail.com>
2021-06-30 20:19:48 +0300

    added 8

--- answers.sql
+++ answers.sql
@@ -32,3 +32,23 @@ SELECT Product.model AS model, price
 FROM Product INNER JOIN 
 Printer ON Printer.model=Product.model WHERE maker = 'B'
 
+
+--8 почти решение
+SELECT DISTINCT maker from Product
+WHERE maker NOT IN
+(SELECT DISTINCT maker from Product
+INNER JOIN Laptop ON Laptop.model=Product.model)
+AND maker IN 
+(SELECT DISTINCT maker from Product
+INNER JOIN PC ON PC.model=Product.model)
+
+--8 
+SELECT DISTINCT maker
+FROM product
+WHERE type = 'pc'
+EXCEPT
+SELECT DISTINCT product.maker
+FROM product
+Where type = 'laptop'
+
+
